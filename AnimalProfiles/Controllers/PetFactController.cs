6c4ac24e58cf3b2d5e4940b996ab663bfa828b1e
Amber Mills <88246893+ambmeals@users.noreply.github.com>
2022-11-22 21:14:46 -0600

    update

--- AnimalProfiles/Controllers/PetFactController.cs
+++ AnimalProfiles/Controllers/PetFactController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using PetFactAPI;
 
 namespace AnimalProfiles.Controllers;
 
--- AnimalProfiles/PetLibrary.cs
+++ AnimalProfiles/PetLibrary.cs
@@ -1,4 +1,4 @@
-﻿namespace PetFactAPI
+﻿namespace AnimalProfiles
 
 {
     public class PetLibrary
